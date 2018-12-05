f8bc0db64c5b34ab3b0dcfa7629a8f7dc7cdfa03
jtr109 <lyp_login@outlook.com>
2018-12-05 21:36:14 +0800

    feat(myhost): stop logging

--- start.sh
+++ start.sh
@@ -9,6 +9,5 @@ docker rm myhost-nginx;
 docker run \
   --name myhost-nginx \
   -v $currentDir/nginx.conf:/etc/nginx/nginx.conf:ro \
-  -v $currentDir/log/crawler.access.log:logs/crawler.access.log
   -p 80:80 \
   nginx
